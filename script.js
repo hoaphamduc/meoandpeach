a50c8e902b6196ac0512641e8d710effb0734eb7
hoaphamduc <hoaphamduc2399@gmail.com>
2024-05-21 00:35:55 +0700

    hề hề

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var welcomeElement = document.getElementById("welcome");
-// var text = "Tính đến hôm nay Mèo và Pít đã yêu nhau được " + countDays('2023-08-18') + " ngày. Hãy xem lại hành trình của chúng mình trong thời gian qua nhé!";
-var text = "Cuộc hành trình của chúng mình đã dừng lại ngày 14/05/2024. Cảm ơn em vì 9 tháng qua đã luôn đồng hành cùng anh, lắng nghe, chia sẻ, quan tâm chăm sóc cho anh. Dù mai sau có như thế nào, anh cũng chúc em luôn được hạnh phúc. Hãy xem lại hành trình của chúng mình trong thời gian qua nhé!";
+var text = "Tính đến hôm nay Mèo và Pít đã yêu nhau được " + countDays('2023-08-18') + " ngày. Hãy xem lại hành trình của chúng mình trong thời gian qua nhé!";
+// var text = "Cuộc hành trình của chúng mình đã dừng lại ngày 14/05/2024. Cảm ơn em vì 9 tháng qua đã luôn đồng hành cùng anh, lắng nghe, chia sẻ, quan tâm chăm sóc cho anh. Dù mai sau có như thế nào, anh cũng chúc em luôn được hạnh phúc. Hãy xem lại hành trình của chúng mình trong thời gian qua nhé!";
 
 welcomeElement.innerHTML = "";
 
